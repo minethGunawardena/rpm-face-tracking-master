a14ffd2ebdac142324a868a56cd2ca2c7b251206
Mineth Gunawardena <minethdilshangunawardena2002@gmail.com>
2025-04-18 23:13:32 +0530

    bug fixing

--- src/App.tsx
+++ src/App.tsx
@@ -152,11 +152,8 @@ function App() {
   };
 
   useEffect(() => {
-    const initialize = async () => {
-      await setup();
-    };
-    initialize();
-  }, [])
+    setup();
+  }, [setup]);
 
   const presetAvatars = [
     "https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024",
